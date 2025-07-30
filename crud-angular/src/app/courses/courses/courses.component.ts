cb5bfca6b57e60eaf6a1c0b487af7afe7542b6fa
Guilherme Luan <guilhermeluan@pm.me>
2025-07-30 16:51:49 -0300

    07 - CSS do Material Table e Criando um Módulo App Material

--- crud-angular/src/app/courses/courses/courses.component.ts
+++ crud-angular/src/app/courses/courses/courses.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Course} from '../model/course';
 
 @Component({
   selector: 'app-courses',
   standalone: false,
   templateUrl: './courses.component.html',
-  styleUrl: './courses.component.scss'
+  styleUrl: './courses.component.scss',
 })
 export class CoursesComponent {
+  courses: Course[] = [
+    {_id: '1', name: 'Angular Fundamentals', category: 'Development'},
+  ];
+  displayedColumns: string[] = ['name', 'category'];
+
 
 }
